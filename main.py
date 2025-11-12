041f1340e3b2a5fc17e115c4507da6d701b3601e
Brian Tuohy <BTcodez22@gmail.com>
2025-11-12 09:55:33 -0600

    d1: added asteroid/asteroid spawn logic

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(
         x = SCREEN_WIDTH / 2, 
         y= SCREEN_HEIGHT / 2
     )
+    af = AsteroidField()
 
     while True:
         log_state()
@@ -20,8 +29,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for i in drawable:
+            i.draw(screen)
+#        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         #print(dt)    
