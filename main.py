5c460f7c211df19bb0098a8f9ef988eb100c5ee3
Brian Tuohy <BTcodez22@gmail.com>
2025-11-13 15:41:14 -0600

    d3: asteroid collision update/shot logic

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import *
 
 def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -18,26 +20,38 @@ def main():
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
+    Shot.containers = (shots,updatable, drawable)
     player = Player(
         x = SCREEN_WIDTH / 2, 
         y= SCREEN_HEIGHT / 2
     )
     af = AsteroidField()
 
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         updatable.update(dt)
+
         for i in drawable:
             i.draw(screen)
-        for x in asteroids:
-            if x.collides_with(player):
-                log_event("player_hit")
-                print("Game Over!")
-                sys.exit()
+            
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(player):
+                    log_event("player_hit")
+                    print("Game Over!")
+                    sys.exit()
+                elif shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         #print(dt)    
